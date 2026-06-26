cc23da61223e046a605031d6626af5b8f477d8c4
Jonas Santana <jonas.santana@example.com>
2026-06-26 07:23:02 -0400

    Auto-sync

--- Iniciar_Agenda.bat
+++ Iniciar_Agenda.bat
@@ -1,6 +1,10 @@
-﻿@echo off
+@echo off
 title Servidor Agenda (Pode Minimizar)
 cd /d "%~dp0"
+echo Sincronizando agenda com o celular (Nuvem)...
+git add .
+git commit -m "Auto-sync"
+git push origin gh-pages
 echo Iniciando servidor local para a Agenda...
 start http://localhost:8000
 python -m http.server 8000
