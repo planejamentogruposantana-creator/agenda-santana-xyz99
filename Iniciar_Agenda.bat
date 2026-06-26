9dc882e532f849e0945ba4fb09526c0e1477483c
Jonas Santana <jonas.santana@example.com>
2026-06-26 16:52:15 -0400

    Feat: Add sorting feature and fix git pull in startup

--- Iniciar_Agenda.bat
+++ Iniciar_Agenda.bat
@@ -1,9 +1,10 @@
 @echo off
 title Servidor Agenda (Pode Minimizar)
 cd /d "%~dp0"
-echo Sincronizando agenda com o celular (Nuvem)...
+echo Sincronizando agenda com a nuvem do GitHub...
+git pull --rebase --autostash origin gh-pages
 git add .
-git commit -m "Auto-sync"
+git commit -m "Auto-sync local"
 git push origin gh-pages
 echo Iniciando servidor local para a Agenda...
 start http://localhost:8000
